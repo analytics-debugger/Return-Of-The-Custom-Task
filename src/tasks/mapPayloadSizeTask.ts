3c05bda05fd8d78492a173da2ad37df1ed28c3ab
David Vallejo <thyngster@gmail.com>
2024-09-05 20:25:30 +0200

    mapPayloadSizeTask

---
+++ src/tasks/mapPayloadSizeTask.ts
@@ -0,0 +1,32 @@
+// src/tasks/mapPayloadSizeTask.ts
+
+import { RequestPayload } from '../types/RequestPayload';
+
+/**
+ * Adds a event parameter on the provided event property name with the size of the payload.
+ * 
+ * @param payload - The payload object to be modified.
+ * @param name - The name to be used as part of the new key in the payload.
+ * @param scope - The scope determines the prefix of the new key. Defaults to 'event'.
+ * @returns The modified payload object.
+ */
+const mapPayloadSizeTask = (
+    payload: RequestPayload,
+    name: string,
+    scope: 'event'
+): RequestPayload => {
+    if (!payload) {
+        throw new Error('Payload is required.');
+    }
+
+    if (!name) {
+        throw new Error('Name is required.');
+    }
+
+    const key = `epn.${name}`;
+    payload[key] = new URLSearchParams(payload).toString().length;
+
+    return payload;
+};
+
+export default mapPayloadSizeTask;
