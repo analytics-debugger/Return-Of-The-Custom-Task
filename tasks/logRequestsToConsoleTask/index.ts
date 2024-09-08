164ad98e9abb81856c3ae8b9231e1130195ce371
David Vallejo <thyngster@gmail.com>
2024-09-08 16:06:59 +0200

    moved logTask to request

--- tasks/logRequestsToConsoleTask/index.ts
+++ tasks/logRequestsToConsoleTask/index.ts
@@ -7,7 +7,18 @@ import { RequestModel } from "../../types/RequestModel";
  * @returns The modified payload object.
  */
 const logRequestsToConsoleTask = (request: RequestModel): RequestModel => {
-  console.log("ReturnOfTheCustomTask: Request", request);
+  console.group(`%cReturnOfTheCustomTask: New Request ( ${request.events.length} Events )`, "color: purple; font-size: large; font-weight: bold;");
+  console.log("Endpoint", request.endpoint);
+  console.log("Shared Payload", request.sharedPayload);
+  request.events.forEach((e,i)=>{
+    console.group(`#${i+1} - Event: ${e.en}`);
+    console.log(JSON.stringify(e, null, 4));    
+    console.groupEnd();        
+  });
+  console.groupCollapsed("RAW INFO");
+  console.log(request);
+  console.groupEnd();        
+  console.groupEnd();
   return request;
 };
 
