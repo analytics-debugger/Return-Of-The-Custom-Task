647a94e632961061f2649d66fba1907bc4b2e8c7
David Vallejo <thyngster@gmail.com>
2024-09-14 14:13:06 +0100

    move isGA4Hit to helpers

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
+import isGA4Hit from './helpers/isGA4Hit'
 let interceptors: Interceptor[] = [];
-
-// Check if the URL belongs to GA4
-function isGA4Hit(url: string): boolean {
-    try {
-        const urlObj = new URL(url);
-        const params = new URLSearchParams(urlObj.search);
-
-        const tid = params.get('tid');
-        const cid = params.get('cid');
-        const v = params.get('v');
-
-        return !!tid && tid.startsWith('G-') && !!cid && v === '2';
-    } catch (e) {
-        console.error('Error parsing URL:', e);
-        return false;
-    }
-}
 
 // Interceptor function to handle fetch requests and responses
 function interceptor(fetch: typeof window.fetch, args: FetchArgs): Promise<Response> {
